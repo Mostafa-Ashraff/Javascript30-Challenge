a7c0290f5ab9859439919d459a136b24532bcb8a
Mostafa-Ashraff <94311378+Mostafa-Ashraff@users.noreply.github.com>
2022-07-03 23:37:13 +0200

    text animation

--- day-5/main.js
+++ day-5/main.js
@@ -1,11 +1,20 @@
-panels = document.querySelectorAll('.panel');
+let panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => {
     panel.addEventListener('mouseenter', e=>{
         panels.forEach(panel=>{
-            panel.classList.add('unfocused')
+            panel.classList.add('unfocused');
+            let paragraphs = Array.from(panel.querySelectorAll('p'));
+            paragraphs[0].style.cssText= 'display:hidden; transform: translateY(-180px)'
+            paragraphs[2].style.cssText= 'display:hidden; transform: translateY(180px)';
         })
         panel.classList.remove('unfocused');
         panel.classList.add('focused');
+        let paragraphs = Array.from(panel.querySelectorAll('p'));
+        paragraphs[0].style.cssText= 'display:block; transform: translateY(0px)';
+        paragraphs[2].style.cssText= 'display:block; transform: translateY(0px)';
+        
+        
+
     })
 });
